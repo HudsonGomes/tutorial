127bb9350299ba044fd10b5bd4036174f7aa187d
hudson <hudson.sgomes@gmail.com>
2016-02-24 20:09:35 -0300

    Second commit

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -85,7 +85,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT= os.path.join(PROJECT_ROOT,'static/')
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = ()
 
 # SwampDragon settings
